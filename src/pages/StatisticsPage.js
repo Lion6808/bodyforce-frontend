2721000b1c66529f6267e8e46a1679aef724244a
Lion6808 <l.sutkowski@gmail.com>
2025-10-27 12:12:32 +0100

    Maj Statistics

--- src/pages/StatisticsPage.js
+++ src/pages/StatisticsPage.js
@@ -270,12 +270,7 @@ export default function StatisticsPage() {
           value={stats?.femmes || 0}
           subtitle={`${stats?.total ? ((stats.femmes / stats.total) * 100).toFixed(0) : 0}%`}
         />
-        //<StatCard
-        //  icon={<FaEuroSign className="text-green-600 text-3xl" />}
-        //  label="Revenus"
-        //  value={`${(revenueTotal || 0).toFixed(0)}€`}
-        //  subtitle="total des paiements"
-        />
+       
       </div>
 
       {/* 🔹 Graphiques : Présences / Genre */}
